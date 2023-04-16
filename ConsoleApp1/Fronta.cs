6e90e4a7e163d68d182f768efce8ad606f8b3390
Dominik Husák <domca.husak@gmail.com>
2023-04-16 09:39:48 +0200

    commit

---
+++ ConsoleApp1/Fronta.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Fronta
+    {
+        private Queue<int> queue = new Queue<int>();
+        private object lockObj = new object(); // Zámek pro synchronizaci přístupu k frontě
+
+        // Metoda pro vložení prvku do fronty
+        public void VlozPrvek(int cislo)
+        {
+            lock (lockObj) // Použití zámku pro synchronizaci
+            {
+                queue.Enqueue(cislo);
+                Console.WriteLine($"Prvek {cislo} byl vložen do fronty.");
+                Monitor.Pulse(lockObj); // Probuzení vlákna, které čeká na prvek
+            }
+        }
+
+        // Metoda pro získání prvku z fronty
+        public int ZiskejPrvek()
+        {
+            lock (lockObj) // Použití zámku pro synchronizaci
+            {
+                while (queue.Count == 0) // Pokud je fronta prázdná, čekáme na prvek
+                {
+                    Monitor.Wait(lockObj); // Uvolnění zámku a uspání vlákna
+                }
+                int prvek = queue.Dequeue();
+                Console.WriteLine($"Prvek {prvek} byl odebrán z fronty.");
+                return prvek;
+            }
+        }
+
+        // Metoda pro zjištění počtu prvků ve frontě
+        public int PocetPrvkuVeFronte()
+        {
+            lock (lockObj) // Použití zámku pro synchronizaci
+            {
+                return queue.Count;
+            }
+        }
+    }
+}
+
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Fronta fronta = new Fronta();
+
+            Thread vlaknoVkladani = new Thread(() =>
+            {
+                Random rnd = new Random();
+                for (int i = 0; i < 10; i++)
+                {
+                    int cislo = rnd.Next(1, 101); // Generování náhodného čísla v rozsahu 1-100
+                    fronta.VlozPrvek(cislo);
+                    Thread.Sleep(500); // Pauza mezi vkládáním prvku
+                }
+            });
+
+            Thread vlaknoOdebirani = new Thread(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    int prvek = fronta.ZiskejPrvek();
+                    Thread.Sleep(1000); // Pauza mezi odebíráním prvku
+                }
+            });
+
+            vlaknoVkladani.Start();
+            vlaknoOdebirani.Start();
+
+            vlaknoVkladani.Join();
+            vlaknoOdebirani.Join();
+
+            Console.WriteLine($"Ve frontě zbylo {fronta.PocetPrvkuVeFronte()} prvků.");
+        }
+    }
+    }
+
